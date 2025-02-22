5139b81c53b87a1adcca9a107bc27da699a0722e
nalinture <68896641+nalinture@users.noreply.github.com>
2025-02-22 17:16:51 +0530

    Update hello.java

--- src/main/java/com/gradlehello/hello.java
+++ src/main/java/com/gradlehello/hello.java
@@ -2,6 +2,6 @@ package com.gradlehello;
 
 public class hello {
     public static void main(String[] args) {
-        System.out.println("Hello! I am Gradle 4tw!");
+        System.out.println("********\nVanakkam _/\_\n********");
     }
 }
