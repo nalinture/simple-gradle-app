04b8da8917d815962a56b8cbc61e1cd25b3e2dec
nalinture <68896641+nalinture@users.noreply.github.com>
2025-02-22 17:22:57 +0530

    Update hello.java

--- src/main/java/com/gradlehello/hello.java
+++ src/main/java/com/gradlehello/hello.java
@@ -2,6 +2,6 @@ package com.gradlehello;
 
 public class hello {
     public static void main(String[] args) {
-        System.out.println("********\nVanakkam _/\_\n********");
+        System.out.println("********This is to test k6********\nVanakkam\n*****Nandri*****");
     }
 }
